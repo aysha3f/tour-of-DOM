ff986a10cab7fab814bf837765fcdd03fddce4e8
aysha3f <tahiaysha220@gmail.com>
2025-05-19 00:34:15 +0600

    set inner html  directly

--- js/append.js
+++ js/append.js
@@ -34,4 +34,14 @@ section.appendChild(ul);
 mainContainer.appendChild(section);
 
 
-//
+// set innerHTML directly 
+const sectionDress = document.createElement('section');
+sectionDress.innerHTML = `
+<h1> My Dress Section </h1>
+<ul>
+   <li> T-Shirt </li>
+   <li> Lungi </li>
+   <li> Sando Genji </li>
+   </ul>
+`
+mainContainer.appendChild(sectionDress);
